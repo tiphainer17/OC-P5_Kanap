d6b7ad540f0e8a97325290b630c01ad1e0fa5154
tiphainer17 <tiphaine.renouf@gmail.com>
2022-10-11 17:24:02 +0200

    debut mise dans le panier

--- front/js/product.js
+++ front/js/product.js
@@ -11,6 +11,9 @@ fetch(urlApi) //requete GET sur l'api
             if (resultat.ok) {
                 resultat.json().then( //convertion données json en javascript
                     function(kanap) {
+                        /****************************************/
+                        /*  AFFICHAGE DES DONNEES SUR LA PAGE   */
+                        /****************************************/
                         document.title=`${kanap.name}`;
                         document.querySelector(".item__img").innerHTML = `<img src="${kanap.imageUrl}" alt="${kanap.altTxt}"> `;
                         document.querySelector("#title").textContent = `${kanap.name}`;
@@ -22,6 +25,58 @@ fetch(urlApi) //requete GET sur l'api
                             options = `<option value="${color}">${color}</option>`;
                             document.querySelector("#colors").insertAdjacentHTML("beforeend",options);
                         }
+
+                        /****************************************/
+                        /*     AJOUT ARTICLE DANS LE PANIER     */
+                        /****************************************/
+                        document.querySelector("#addToCart").addEventListener("click",function(event){ //au clic sur le bouton
+                            event.preventDefault(); //suppression comportement par defaut
+                            if(// si les champs sont bien rempli
+                                document.querySelector("#quantity").reportValidity() &&
+                                document.querySelector("#colors").reportValidity()
+                            ){  
+                                            let produitToAdd = {
+                                                id :kanap._id,
+                                                quantite : parseInt(document.querySelector("#quantity").value),
+                                                color : document.querySelector("#colors").value,
+                                            }; //on creer un objet avec les valeurs saisies
+                                            
+                                            let panier = JSON.parse(localStorage.getItem("panier")); 
+
+                                            if (panier){ //si panier existe
+                                                for(let list of panier){ //pour chaque element du panier on verifie, 
+                                                    if(produitToAdd._id == list.id && produitToAdd.color== list.color) {//si l'objet est deja present
+                                                        let produitToAddDouble={
+                                                            id : list.id,
+                                                            quantite : list.quantite = list.quantite+produitToAdd.quantite,
+                                                            color : list.color,
+                                                        }; //on cumule les quantités
+
+                                                        let index = panier.indexOf(list); //on recupere sa position dans l'array
+                                                        panier.splice(index,1); //on le supprime
+
+                                                        //on le remplace par la combinaison des quantités
+                                                        panier.push(produitToAddDouble);
+                                                        localStorage.setItem("panier",JSON.stringify(panier));
+
+                                                    }
+                                                    else { //sinon on ajoute notre nouvel objet
+                                                        panier.push(produitToAdd)
+                                                        localStorage.setItem("panier",JSON.stringify(panier));
+                                                    }
+                                                }
+                                            }
+                                            else { //sinon on le créer avant d'ajouter au panier
+                                                panier = [];
+                                                panier.push(produitToAdd);
+                                                localStorage.setItem('panier',JSON.stringify(panier));
+                                            }
+
+                            } else { //sinon demande de les remplir
+                                            alert("Veuillez remplir les informations de couleur et quantité");
+                            }
+                        });
+
                     }
                 );
             }

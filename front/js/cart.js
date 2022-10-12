6ed87acdb7e16fa6fb528441d77207abd2b61cc1
tiphainer17 <tiphaine.renouf@gmail.com>
2022-10-12 17:14:21 +0200

    affichage du localstorage dans panier

---
+++ front/js/cart.js
@@ -0,0 +1,77 @@
+/****************************************/
+/*    INITIALISATION VARIABLE UTILE     */
+/****************************************/
+let urlApi="http://localhost:3000/api/products/"//url standard de l'api
+let panier = JSON.parse(localStorage.getItem("panier")); //on recupere le contenu du localstorage
+let quantiteTotal =0;
+let prixTotal = 0;
+let article =``;
+
+/****************************************/
+/*  AFFICHAGE DES DONNEES SUR LA PAGE   */
+/****************************************/
+function affichagePanier(list,kanap){
+    article+=`<article class="cart__item" data-id="${list.id}" data-color="${list.color}">`;
+    article+=`<div class="cart__item__img">`;
+    article+=`<img src="${kanap.imageUrl}" alt="${kanap.altTxt}">`;
+    article+=`</div>`;
+    article+=`<div class="cart__item__content">`;
+    article+=`<div class="cart__item__content__description">`;
+    article+=`<h2>${kanap.name}</h2>`;
+    article+=`<p>${list.color}</p>`;
+    article+=`<p>${list.quantite*kanap.price} €</p>`;
+    article+=`</div>`;
+    article+=`<div class="cart__item__content__settings">`;
+    article+=`<div class="cart__item__content__settings__quantity">`;
+    article+=`<p>Qté : </p>`;
+    article+=`<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${list.quantite}">`;
+    article+=`</div>`;
+    article+=`<div class="cart__item__content__settings__delete">`;
+    article+=`<p class="deleteItem">Supprimer</p>`;
+    article+=`</div>`;
+    article+=`</div>`;
+    article+=`</div>`;
+    article+=`</article>`;
+    document.querySelector("#cart__items").innerHTML=article;//on ajoute le contenu html du kanap actuel
+
+    //on met a jour le total du panier
+    quantiteTotal += list.quantite;
+    prixTotal += list.quantite*kanap.price;
+    document.querySelector("#totalQuantity").textContent = `${quantiteTotal}`;
+    document.querySelector("#totalPrice").textContent=`${prixTotal}`;                         
+}
+
+
+
+//recuperation valeur id de chaque article
+if (panier !==null){ //si le contenu du panier n'est pas vide
+    for(let list of panier){ // on parcours tout le localstorage
+        let urlApiId= urlApi+list.id;//on ajoute a l'url standard le numero de l'id du kanap parcouru actuellement
+        fetch(urlApiId) //on requete
+            .then(
+                function(resultat){
+                    if(resultat.ok){
+                        resultat.json().then(
+                            function(kanap){
+                                affichagePanier(list,kanap);//on affiche les valeurs du kanap actuel
+                            }
+                        )
+                    }
+                }
+            )
+            .catch(
+                function(err){
+                    alert("Veuillez vérifier votre connexion au service web")
+                }
+            )
+        ;
+
+    }
+    
+}
+else {
+    document.querySelector("#cart__items").innerHTML=`<p>Aucun produit ajouté au panier</p>`;
+    document.querySelector("#totalQuantity").textContent = `${quantiteTotal}`;
+    document.querySelector("#totalPrice").textContent=`${prixTotal}`;
+                            
+}
